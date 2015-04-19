66fdace642db57b266d7e250065897ba3fdc157d
Jan Unger <ju@juit.de>
2015-04-19 14:55:02 +0200

    Added a low-level self test console command

---
+++ src/GDCBundle/Command/SelfTestSystemCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GDCBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SelfTestSystemCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('gdc:system:self-test');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $piFace = $this->getContainer()->get('gdc.piface_factory')->createInstance();
+        foreach (range(0, 7) as $i) {
+            $output->write("PIN $i: ");
+            $output->writeln($piFace->getInputPin(0)->isOn() ? 'On' : 'Off');
+        }
+    }
+}
