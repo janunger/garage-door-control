2c1138be0d5ed8c7bbe499905d693a7647f5d998
Jan Unger <ju@juit.de>
2015-05-02 19:51:50 +0200

    Serving app html from a static file

--- src/GDCBundle/Controller/DefaultController.php
+++ src/GDCBundle/Controller/DefaultController.php
@@ -5,21 +5,12 @@ namespace GDCBundle\Controller;
 use GDC\Camera;
 use GDC\CommandQueue\Command;
 use GDCBundle\Entity\CommandQueueEntry;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Template()
-     */
-    public function indexAction()
-    {
-        return [];
-    }
-
     public function triggerAction()
     {
         $repository = $this->get('gdc.command_queue_entry_repository');
