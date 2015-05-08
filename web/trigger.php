5cd480b00cfeb3bd60681115c3f5e7aff0ce9a4c
Jan Unger <ju@juit.de>
2015-05-09 01:19:47 +0200

    Trigger controller can handle sequence 1 command

--- web/trigger.php
+++ web/trigger.php
@@ -1,22 +1,28 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    header('HTTP/1.1 400 Bad Request');
-    echo 'Bad request';
-    exit;
-}
+call_user_func(function () {
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        header('HTTP/1.1 400 Bad Request');
+        echo 'Bad request';
+        exit;
+    }
 
-$dbParameters = require_once __DIR__ . '/../app/config/db_credentials.php';
+    $dbParameters = require_once __DIR__ . '/../app/config/db_credentials.php';
 
-$db = new PDO(
-    'mysql:' . $dbParameters['database_host'] . ';dbname=' . $dbParameters['database_name'] . ';charset=utf8',
-    $dbParameters['database_user'],
-    $dbParameters['database_password']
-);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->exec("USE " . $dbParameters['database_name']);
+    $db = new PDO(
+        'mysql:' . $dbParameters['database_host'] . ';dbname=' . $dbParameters['database_name'] . ';charset=utf8',
+        $dbParameters['database_user'],
+        $dbParameters['database_password']
+    );
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->exec("USE " . $dbParameters['database_name']);
 
-$db->exec("INSERT INTO command_queue (command, created_at) VALUES ('trigger-door', NOW())");
+    $command = 'trigger-door';
+    if (isset($_POST['sequence']) && '1' === $_POST['sequence']) {
+        $command = 'close-after-one-transit';
+    }
+    $db->exec("INSERT INTO command_queue (command, created_at) VALUES ('$command', NOW())");
 
-header('Content-Type: application/json');
-echo '{}';
+    header('Content-Type: application/json');
+    echo '{}';
+});
