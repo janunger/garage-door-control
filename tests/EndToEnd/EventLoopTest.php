476258f63768494c52ae0a1efa7fd773e61285bf
Jan Unger <jan.unger@juit.de>
2017-10-23 00:45:16 +0200

    Watchdog functional

--- tests/EndToEnd/EventLoopTest.php
+++ tests/EndToEnd/EventLoopTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace JUIT\GDC\Tests\EndToEnd;
 
 use JUIT\MailHog\MailHogClient;
+use JUIT\MailHog\Message;
 use JUIT\PiFace\Emulator\InputPin;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Process;
@@ -15,7 +16,7 @@ class EventLoopTest extends TestCase
     private static $eventLoop;
 
     /** @var MailHogClient */
-    private $mailHog;
+    private static $mailHog;
 
     /** @var InputPin */
     private $pifacePinDoorClosed;
@@ -28,32 +29,87 @@ class EventLoopTest extends TestCase
         parent::setUpBeforeClass();
 
         // "exec ..." is required if $process->stop() isn't working without.
-        static::$eventLoop = new Process('exec ');
+        static::$eventLoop = new Process('exec ' . PROJECT_ROOT_DIR . '/bin/event_loop');
+        static::$mailHog   = MailHogClient::create('http://mailhog:8025');
     }
 
     protected function setUp()
     {
         parent::setUp();
 
-        $this->mailHog = MailHogClient::create('http://mailhog:8025');
         $this->pifacePinDoorClosed = new InputPin(0, new \SplFileInfo(EMULATOR_DATA_DIR));
         $this->pifacePinDoorOpened = new InputPin(1, new \SplFileInfo(EMULATOR_DATA_DIR));
 
-        $this->mailHog->deleteAll();
+        static::$mailHog->deleteAll();
     }
 
     /** @test */
-    public function it_should_send_a_mail_with_door_state_on_startup()
+    public function it_sends_a_mail_with_door_state_on_startup()
     {
-        $this->markTestIncomplete();
-
         $this->pifacePinDoorClosed->setOn();
         $this->pifacePinDoorOpened->setOff();
 
-//        static::$eventLoop->start();
+        static::$eventLoop->start();
 
-        $messages = $this->mailHog->fetchAll();
+        $messages = $this->fetchMailHogMessages();
         static::assertCount(1, $messages);
+        static::assertRegExp('/^Watchdog restarted, door closed/', $messages[0]->getSubject());
+    }
+
+    /**
+     * @test
+     * @depends it_sends_a_mail_with_door_state_on_startup
+     */
+    public function it_sends_a_mail_on_door_opening()
+    {
+        $this->pifacePinDoorClosed->setOff();
+
+        $messages = $this->fetchMailHogMessages();
+        static::assertCount(1, $messages);
+        static::assertRegExp('/^DOOR OPENING/', $messages[0]->getSubject());
+    }
+
+    /**
+     * @test
+     * @depends it_sends_a_mail_on_door_opening
+     */
+    public function it_sends_a_mail_on_door_closed_again()
+    {
+        $this->pifacePinDoorClosed->setOn();
+
+        $messages = $this->fetchMailHogMessages();
+        static::assertCount(1, $messages);
+        static::assertRegExp('/^Door closed/', $messages[0]->getSubject());
+    }
+
+    /**
+     * @test
+     * @depends it_sends_a_mail_on_door_closed_again
+     */
+    public function it_sends_a_mail_on_hardware_error()
+    {
+        $this->pifacePinDoorClosed->setOn();
+        $this->pifacePinDoorOpened->setOn();
+
+        $messages = $this->fetchMailHogMessages();
+        static::assertCount(1, $messages);
+        static::assertRegExp('/^HARDWARE ERROR/', $messages[0]->getSubject());
+    }
+
+    /** @return Message[] */
+    private function fetchMailHogMessages($numberToWaitFor = 1, $timeout = 5): array
+    {
+        $start = time();
+        while (true) {
+            $messages = static::$mailHog->fetchAll();
+            if (count($messages) >= $numberToWaitFor) {
+                return $messages;
+            }
+            if (time() > $start + $timeout) {
+                $this->fail('MailHog did not receive the expected number of messages within timeout');
+            }
+            usleep(300000);
+        }
     }
 
     public static function tearDownAfterClass()
@@ -61,6 +117,7 @@ class EventLoopTest extends TestCase
         if (self::$eventLoop->isStarted()) {
             self::$eventLoop->stop();
         }
+        static::$mailHog->deleteAll();
 
         parent::tearDownAfterClass();
     }
