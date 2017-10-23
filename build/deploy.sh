fce0498880366485fd8c69ea7ae52a9abbaad26f
Jan Unger <jan.unger@juit.de>
2017-10-23 04:21:40 +0200

    Configured supervisor

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rsync -avz --delete ../ gdc:/home/pi/gdc/ --exclude-from=./deployment_exclusions
+rsync -avz --delete --no-perms --no-owner --no-group ../ gdc:/var/www/ --exclude-from=./deployment_exclusions
+sudo supervisorctl restart gdc
