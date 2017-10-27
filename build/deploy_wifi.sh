43c9b2ff19c21d0f2114fbc7e65631ed694aeffa
Jan Unger <jan.unger@juit.de>
2017-10-27 22:48:00 +0200

    SSH alias for dev Raspi

--- build/deploy_wifi.sh
+++ build/deploy_wifi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rsync -avz --delete --no-perms --no-owner --no-group ../ pi@192.168.73.11:/var/www/ --exclude-from=./deployment_exclusions
+rsync -avz --delete --no-perms --no-owner --no-group ../ gdc-wifi:/var/www/ --exclude-from=./deployment_exclusions
 ssh gdc << EOF
     set -xe
     sudo supervisorctl restart gdc
