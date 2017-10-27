ea1082e011b4158201ee19b5b557753b48c7f469
Jan Unger <jan.unger@juit.de>
2017-10-27 22:56:18 +0200

    Preparing for more tests with database involved

---
+++ tests/DatabaseTestCase.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JUIT\GDC\Tests;
+
+use PDO;
+use PHPUnit\DbUnit\Database\Connection;
+use PHPUnit\DbUnit\DataSet\DefaultDataSet;
+use PHPUnit\DbUnit\DataSet\FlatXmlDataSet;
+use PHPUnit\DbUnit\Operation\Factory;
+use PHPUnit\DbUnit\TestCase;
+
+class DatabaseTestCase extends TestCase
+{
+    /**
+     * Returns the test database connection.
+     *
+     * @return Connection
+     */
+    protected function getConnection()
+    {
+        $config   = require PROJECT_ROOT_DIR . '/etc/config.php';
+        $dbName   = $config['database_name'];
+        $host     = $config['database_host'];
+        $username = $config['database_username'];
+        $password = $config['database_password'];
+
+        $pdo = new PDO(
+            "mysql:dbname={$dbName};host={$host}",
+            $username,
+            $password,
+            [
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            ]
+        );
+
+        return $this->createDefaultDBConnection($pdo, $dbName);
+    }
+
+    /**
+     * Returns the test dataset.
+     *
+     * @return DefaultDataSet
+     */
+    protected function getDataSet()
+    {
+        return new DefaultDataSet();
+    }
+
+    protected function getTearDownOperation()
+    {
+        return Factory::TRUNCATE();
+    }
+}
--- tests/Integration/ControlSequence/RepositoryTest.php
+++ tests/Integration/ControlSequence/RepositoryTest.php
@@ -7,14 +7,10 @@ namespace JUIT\GDC\Tests\Integration\ControlSequence;
 use JUIT\GDC\ControlSequence\Command;
 use JUIT\GDC\ControlSequence\Repository;
 use JUIT\GDC\ControlSequence\SequenceItem;
-use PDO;
-use PHPUnit\DbUnit\Database\Connection;
-use PHPUnit\DbUnit\DataSet\DefaultDataSet;
+use JUIT\GDC\Tests\DatabaseTestCase;
 use PHPUnit\DbUnit\DataSet\FlatXmlDataSet;
-use PHPUnit\DbUnit\Operation\Factory;
-use PHPUnit\DbUnit\TestCase;
 
-class RepositoryTest extends TestCase
+class RepositoryTest extends DatabaseTestCase
 {
     /** @var Repository */
     private $SUT;
@@ -51,44 +47,8 @@ class RepositoryTest extends TestCase
         static::assertTablesEqual($expected, $actual);
     }
 
-    /**
-     * Returns the test database connection.
-     *
-     * @return Connection
-     */
-    protected function getConnection()
-    {
-        $config = require PROJECT_ROOT_DIR . '/etc/config.php';
-        $dbName   = $config['database_name'];
-        $host     = $config['database_host'];
-        $username = $config['database_username'];
-        $password = $config['database_password'];
-
-        $pdo = new PDO(
-            "mysql:dbname={$dbName};host={$host}",
-            $username,
-            $password,
-            [
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            ]
-        );
-
-        return $this->createDefaultDBConnection($pdo, $dbName);
-    }
-
-    /**
-     * Returns the test dataset.
-     *
-     * @return DefaultDataSet
-     */
     protected function getDataSet()
     {
         return new FlatXmlDataSet(__DIR__ . '/Repository/seed.xml');
-    }
-
-    protected function getTearDownOperation()
-    {
-        return Factory::TRUNCATE();
     }
 }
