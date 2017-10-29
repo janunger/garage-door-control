df3772a8053546ea9991ef54d59bb67342312853
Jan Unger <jan.unger@juit.de>
2017-10-29 21:24:39 +0100

    Fixed tests

--- tests/DatabaseTestCase.php
+++ tests/DatabaseTestCase.php
@@ -7,7 +7,6 @@ namespace JUIT\GDC\Tests;
 use PDO;
 use PHPUnit\DbUnit\Database\Connection;
 use PHPUnit\DbUnit\DataSet\DefaultDataSet;
-use PHPUnit\DbUnit\DataSet\FlatXmlDataSet;
 use PHPUnit\DbUnit\Operation\Factory;
 use PHPUnit\DbUnit\TestCase;
 
@@ -21,10 +20,10 @@ class DatabaseTestCase extends TestCase
     protected function getConnection()
     {
         $config   = require PROJECT_ROOT_DIR . '/etc/config.php';
-        $dbName   = $config['database_name'];
-        $host     = $config['database_host'];
-        $username = $config['database_username'];
-        $password = $config['database_password'];
+        $dbName   = $config['database']['name'];
+        $host     = $config['database']['host'];
+        $username = $config['database']['username'];
+        $password = $config['database']['password'];
 
         $pdo = new PDO(
             "mysql:dbname={$dbName};host={$host}",
