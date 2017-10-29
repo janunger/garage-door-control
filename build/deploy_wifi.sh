0c2fcb5f16fc31ff0b247ce774d00c1786c811d2
Jan Unger <jan.unger@juit.de>
2017-10-29 21:16:53 +0100

    Dependency update, minor deployment fix

--- build/deploy_wifi.sh
+++ build/deploy_wifi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rsync -avz --delete --no-perms --no-owner --no-group ../ gdc-wifi:/var/www/ --exclude-from=./deployment_exclusions
-ssh gdc << EOF
+ssh gdc-wifi << EOF
     set -xe
     sudo supervisorctl restart gdc
 EOF
